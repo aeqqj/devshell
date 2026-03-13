8db25588fd8c69ffba3d6e918713ac9f49c49ed7
aeqqj <48713075+aeqqj@users.noreply.github.com>
2026-03-13 21:39:47 +0800

    added tmux back

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+mkdir -p ~/.config
 
-cp ~/devshell/.zshrc .zshrc
-cp -r ~/devshell/nvim .config/nvim
+cp ~/devshell/.zshrc ~/.zshrc
+cp -r ~/devshell/nvim ~/.config/nvim
+cp -r ~/devshell/tmux ~/.config/tmux
 cp ~/devshell/starship.toml ~/.config/starship.toml
