fde262eb14375ea898c696dbdb42060357919bbf
aeqqj <48713075+aeqqj@users.noreply.github.com>
2026-03-20 16:45:32 +0800

    tmux keymaps fix

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ mkdir -p ~/.config
 cp ~/devshell/.zshrc ~/.zshrc
 cp -r ~/devshell/nvim ~/.config/nvim
 cp -r ~/devshell/tmux ~/.config/tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ~/devshell/starship.toml ~/.config/starship.toml
